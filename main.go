b8019a2c4d5f2ef7b4dc87adc662c4de1a95d886
haru <undermary@gmail.com>
2024-12-03 12:39:38 +0900

    テストコミット

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-    router := gin.Default()
-    router.GET("/hello/:name",func(c *gin.Context){
-        name := c.Param("name")
-        c.String(http.StatusOK, makeGreeting(name))
-    })
-    router.Run(":8080")
+	router := gin.Default()
+	router.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, makeGreeting(name))
+	})
+	router.Run(":8080")
 }
 func makeGreeting(name string) string {
-    return fmt.Sprintf("Hello %d!", name)
+	return fmt.Sprintf("Hello %d!", name)
 }
--- main_test.go
+++ main_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
-func TestMakeGreeting(t *testing.T){
-    want :="Hello, haru0121!"
-    got := makeGreeting("haru0121")
-    if got != want {
-        t.Errorf("want %s, but got %s", want, got)
-    }
+func TestMakeGreeting(t *testing.T) {
+	want := "Hello, haru0121!"
+	got := makeGreeting("haru0121")
+	if got != want {
+		t.Errorf("want %s, but got %s", want, got)
+	}
 }
